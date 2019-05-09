b79b399d52a42e5aa6ab1e6ba840e8a403acb81a
percy rojas <newper212@gmail.com>
2019-05-09 09:52:09 -0500

    modificacion puerto servidor2

--- index.js
+++ index.js
@@ -45,4 +45,4 @@ app.post('/uploadfile', upload.single('filePhoto'), (req, res, next) => {
 
      })
 
-app.listen(process.env.PORT || 3000, () => console.log('Server started on port 3000'));
+app.listen(process.env.port || 1337, () => console.log('Server started on port 3000'));
